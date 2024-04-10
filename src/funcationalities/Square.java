b19bfa75a15003be4c618f5ce36eecd6cac53d46
Jayash3812 <114846790+Jayash3812@users.noreply.github.com>
2024-04-11 00:14:40 +0530

    Update Square.java

--- src/funcationalities/Square.java
+++ src/funcationalities/Square.java
@@ -6,7 +6,7 @@ public class Square {
 	
 	public static void square() {
 		
-		Scanner scanner = new Scanner(System.in);
+	Scanner scanner = new Scanner(System.in);
         System.out.print("Enter the number: ");
         double num = scanner.nextDouble();
         System.out.println("Square is: " + (num * num));
