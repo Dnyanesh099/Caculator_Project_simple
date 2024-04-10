0d8e49eccd404a8e9bb0c7a5efacc75c94e4906b
dnyanesh <dnyaneshpatil556@gmail.com>
2024-04-10 19:54:56 +0530

    Cube class Addesd

---
+++ src/funcationalities/Cube.java
@@ -0,0 +1,24 @@
+package funcationalities;
+
+import java.util.Scanner;
+
+import validitions.DataTypeChecker;
+
+public class Cube {
+	public static void cube() {
+
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter the number>> ");
+		double num = scanner.nextDouble();
+
+		double result = num * num * num;
+
+		if (DataTypeChecker.numcheck(num)) {
+
+			System.out.println("Cube is>> " + (int) result);
+		} else {
+			System.out.println("Cube is>> " + result);
+		}
+	}
+
+}
