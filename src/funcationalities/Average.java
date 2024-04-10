3c3ed9351233c59278f2850e7bb1993c4ea2cd28
dnyanesh <dnyaneshpatil556@gmail.com>
2024-04-10 19:47:12 +0530

    Average class Added

---
+++ src/funcationalities/Average.java
@@ -0,0 +1,29 @@
+package funcationalities;
+
+import java.util.Scanner;
+
+public class Average {
+	static double sum = 0, a, avg;
+
+	public static void average() {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the limit>>");
+
+		int n = sc.nextInt();
+
+		if (n != 0) {
+
+			for (int i = 1; i <= n; i++) {
+				System.out.println("Enter the Number " + i + ">> ");
+				a = sc.nextInt();
+				sum = sum + a;
+				avg = sum / n;
+			}
+
+		} else {
+			System.out.println("Please enter the valid limit....");
+		}
+
+	}
+}
